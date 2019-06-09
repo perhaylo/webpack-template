1a9f40eed3257d05dd473de6bc0ec860aa04e1d3
Andrew Perhaylo <perhaylo@gmail.com>
2019-06-09 14:46:07 +0300

    Update

--- webpack.config.js
+++ webpack.config.js
@@ -3,28 +3,27 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'public/js'),
-    filename: 'main.js',
-    publicPath: 'js/'
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
   },
   devServer: {
-    contentBase: path.join(__dirname, 'public'),
+    contentBase: path.join(__dirname, 'dist'),
+    disableHostCheck: true,
     compress: true,
     port: 8080
   },
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.(js|jsx)$/,
         loader: 'babel-loader',
         exclude: /node_modules/
-      },
-      {
+      }, {
         test: /\.scss$/,
         use: [
-          "style-loader", // 3) creates style nodes from JS strings
-          "css-loader", // 2) translates CSS into CommonJS
-          "sass-loader" // 1) compiles Sass to CSS, using Node Sass by default
+          'style-loader', // 3) creates style nodes from JS strings
+          'css-loader', // 2) translates CSS into CommonJS
+          'sass-loader' // 1) compiles Sass to CSS, using Node Sass by default
         ]
       }
     ]
